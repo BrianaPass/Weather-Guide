deeec57f953f5086b205b1253a758225c5460aa0
BrianaPass <116980413+BrianaPass@users.noreply.github.com>
2023-02-02 19:50:48 -0500

    updated js

---
+++ Weather-Guide/assets/index.js
@@ -0,0 +1,6 @@
+localStorage.clear();
+
+function findCity() {
+    var cityName = titleCase($("#cityName")[0].value.trim());
+
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
